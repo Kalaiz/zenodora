5f73f7d40a3bda930304929f7537c4b087d95aa6
kalai <ssokalai@gmail.com>
2021-07-18 00:05:25 +0800

    feat: Adding quick panel skeleton

--- zenodora/app/src/main/java/com/kalai/zenodora/EntryActivity.kt
+++ zenodora/app/src/main/java/com/kalai/zenodora/EntryActivity.kt
@@ -1,12 +1,20 @@
 package com.kalai.zenodora
 
-import androidx.appcompat.app.AppCompatActivity
+
 import android.os.Bundle
 import android.view.View
+import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.WindowCompat.*
 import androidx.core.view.WindowInsetsCompat
 import androidx.core.view.WindowInsetsControllerCompat
+import androidx.navigation.Navigation
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.NavigationUI.setupActionBarWithNavController
+import androidx.navigation.ui.NavigationUI.setupWithNavController
+import androidx.navigation.ui.setupActionBarWithNavController
 import com.kalai.zenodora.databinding.ActivityEntryBinding
+
 
 class EntryActivity : AppCompatActivity() {
 
@@ -17,9 +25,13 @@ class EntryActivity : AppCompatActivity() {
         binding = ActivityEntryBinding.inflate(layoutInflater)
         val rootView = binding.root
         setContentView(rootView)
-        // Hide the status bar.
 
-        hideSystemUI(rootView)
+
+    }
+    override fun onSupportNavigateUp(): Boolean {
+        val navHostFragment = supportFragmentManager.findFragmentById(R.id.nav_host_fragment) as NavHostFragment
+        val navController = navHostFragment.navController
+        return navController.navigateUp() || super.onSupportNavigateUp()
     }
 
 
@@ -27,9 +39,11 @@ class EntryActivity : AppCompatActivity() {
     private fun hideSystemUI(container:View) {
 
         WindowInsetsControllerCompat(window, container).let { controller ->
-            controller.hide( WindowInsetsCompat.Type.navigationBars() or WindowInsetsCompat.Type.statusBars())
+            controller.hide( WindowInsetsCompat.Type.statusBars() or WindowInsetsCompat.Type.navigationBars())
             controller.systemBarsBehavior = WindowInsetsControllerCompat.BEHAVIOR_SHOW_TRANSIENT_BARS_BY_SWIPE
         }
     setDecorFitsSystemWindows(window, false)
     }
+
+
 }
--- zenodora/app/src/main/java/com/kalai/zenodora/HomeFragment.kt
+++ zenodora/app/src/main/java/com/kalai/zenodora/HomeFragment.kt
@@ -1,5 +1,29 @@
 package com.kalai.zenodora
 
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.navigation.findNavController
+import com.kalai.zenodora.databinding.FragmentHomeBinding
 
-class HomeFragment: Fragment(R.layout.fragment_home) 
+class HomeFragment: Fragment(R.layout.fragment_home){
+    private  lateinit var binding: FragmentHomeBinding
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+      binding = FragmentHomeBinding.inflate(inflater, container, false)
+        binding.settingsImageView.setOnClickListener {
+                view ->
+            view.findNavController().navigate(R.id.action_homeFragment_to_settingsFragment)
+        }
+        
+        return  binding.root
+
+    }
+
+
+}
